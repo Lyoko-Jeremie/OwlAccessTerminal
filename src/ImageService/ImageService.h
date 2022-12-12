44dfe3ac9dabbe482e094995733229d45d392a2c
LyokoJeremie <lucheng989898@gmail.com>
2022-12-12 15:15:12 +0800

    CommonTcpPackage

--- src/ImageService/ImageService.h
+++ src/ImageService/ImageService.h
@@ -10,22 +10,58 @@
 #include <boost/asio/executor.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/strand.hpp>
+#include <boost/asio/streambuf.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/asio/read_until.hpp>
+#include <boost/array.hpp>
 #include <boost/log/trivial.hpp>
 
 namespace OwlImageService {
+
+    struct CommonTcpPackage {
+        uint32_t size = 0;
+        boost::asio::streambuf data_{};
+
+// https://stackoverflow.com/questions/7556327/int-char-conversion-in-network-frames-with-c-boostasio
+//        // sender
+//        // char 4 => uint
+//        char char_len[4];
+//        char_len[0] = (len >> 0);
+//        char_len[1] = (len >> 8);
+//        char_len[2] = (len >> 16);
+//        char_len[3] = (len >> 24);
+//        std::cout << "char[4] len=["
+//        << char_len[0] << ',' << char_len[1] << ','
+//        << char_len[2] << ',' << char_len[3] << ']'
+//        << std::endl;
+//        uint32_t uint_len = *(reinterpret_cast<uint32_t *>( char_len ));
+//        std::cout << "uint len=" << uint_len << std::endl;
+//        // network bytes order
+//        uint32_t net_len = htonl( len );
+//        std::cout << "net_len=" << net_len << std::endl;
+
+//        // receiver
+//        uint32_t net_len;
+//        size_t len_len = sock->read_some( boost::asio::buffer( reinterpret_cast<char*>(&net_len), 4), error );
+//        uint32_t len = ntohl( net_len );
+//        std::cout << "uint len=" << len << std::endl;
+
+        CommonTcpPackage() = default;
+    };
 
     class ImageServiceSession : public std::enable_shared_from_this<ImageServiceSession> {
     public:
         explicit ImageServiceSession(
                 boost::asio::ip::tcp::socket &&socket
-        ) : socket(std::move(socket)) {}
+        ) : socket_(std::move(socket)) {}
 
         static void fail(boost::system::error_code ec, const char *what) {
             BOOST_LOG_TRIVIAL(error) << what << ": " << ec.message();
         }
 
     private:
-        boost::asio::ip::tcp::socket socket;
+        boost::asio::ip::tcp::socket socket_;
+        CommonTcpPackage package_r_{};
 
     public:
         void
@@ -36,7 +72,31 @@ namespace OwlImageService {
     private:
         void
         do_receive() {
+            boost::asio::async_read(
+                    socket_,
+                    boost::asio::buffer(
+                            reinterpret_cast<char *>(&package_r_.size), 4
+                    ),
+                    [this, self = shared_from_this()](
+                            const boost::system::error_code &ec,
+                            std::size_t bytes_transferred) {
 
+                        if (ec == boost::asio::error::eof) {
+                            // Connection closed cleanly by peer.
+                            return;
+                        }
+                        if (ec) {
+                            // Connection error
+                            return;
+                        }
+
+                        package_r_.size = ntohl(package_r_.size);
+
+
+
+                        return;
+                    }
+            );
         }
 
     };
@@ -45,7 +105,7 @@ namespace OwlImageService {
     public:
         ImageService(
                 boost::asio::io_context &ioc,
-                const boost::asio::ip::tcp::endpoint& endpoint
+                const boost::asio::ip::tcp::endpoint &endpoint
         ) : ioc_(ioc),
             acceptor_strand_(boost::asio::make_strand(ioc)),
             acceptor_(acceptor_strand_) {
