22b7f46b8bca6ea80c0ad736a55a769b967c2d43
LyokoJeremie <lucheng989898@gmail.com>
2023-02-15 09:40:56 +0800

    doing OwlMailDefine::AdditionCmd::AprilTag

--- src/CommandService/SerialController.cpp
+++ src/CommandService/SerialController.cpp
@@ -249,30 +249,60 @@ namespace OwlSerialController {
                                                << " switch (data->additionCmd) OwlMailDefine::AdditionCmd::AprilTag"
                                                << " not impl";
 
+                    if (!data->aprilTagCmdPtr) {
+                        BOOST_LOG_TRIVIAL(error) << "SerialController"
+                                                 << " receiveMail"
+                                                 << " switch (data->additionCmd) OwlMailDefine::AdditionCmd::AprilTag"
+                                                 << " (!data->aprilTagCmdPtr)";
+                        return;
+                    }
+
+                    auto pcx = static_cast<uint16_t>(data->aprilTagCmdPtr->imageCenterX);
+                    auto pcy = static_cast<uint16_t>(data->aprilTagCmdPtr->imageCenterY);
+
+                    auto center = data->aprilTagCmdPtr->aprilTagCenter;
+                    if (!center) {
+                        BOOST_LOG_TRIVIAL(error) << "SerialController"
+                                                 << " receiveMail"
+                                                 << " switch (data->additionCmd) OwlMailDefine::AdditionCmd::AprilTag"
+                                                 << " (!center)";
+                        return;
+                    }
+                    auto x = center->cornerLTx - center->cornerLBx;
+                    auto y = center->cornerLTy - center->cornerLBy;
+                    auto r = atan2(y, x);
+                    // rad(+PI ~ -PI) -> degree(+180 ~ -180) -> degree(+360 ~ 0)
+                    auto d = static_cast<uint16_t>(((r / (M_PI / 180.0)) + 180));
+
+                    auto id = static_cast<uint16_t>(center->id);
+                    auto cx = static_cast<uint16_t>(center->centerX);
+                    auto cy = static_cast<uint16_t>(center->centerY);
+
+                    // TODO
+
                     // send cmd to serial
                     auto sendDataString = std::make_shared<std::vector<uint8_t>>();
-//                // 0xAA,0xAdditionCmd,0xXXXX,0xYYYY,0xZZZZ,0xCWCW,0xBB
-//                sendDataString->resize(11);
-//                // make send data
-//                // 0xAA
-//                (*sendDataString).at(0) = char(0xAA);
-//                // AdditionCmd
-//                (*sendDataString).at(1) = uint8_t(data->additionCmd);
-//                // 0xXX
-//                (*sendDataString).at(2) = uint8_t(uint16_t(data->x) & 0xff);
-//                (*sendDataString).at(3) = uint8_t(uint16_t(data->x) >> 8);
-//                // 0xYY
-//                (*sendDataString).at(4) = uint8_t(uint16_t(data->y) & 0xff);
-//                (*sendDataString).at(5) = uint8_t(uint16_t(data->y) >> 8);
-//                // 0xZZ
-//                (*sendDataString).at(6) = uint8_t(uint16_t(data->z) & 0xff);
-//                (*sendDataString).at(7) = uint8_t(uint16_t(data->z) >> 8);
-//                // 0xCW
-//                (*sendDataString).at(8) = uint8_t(uint16_t(data->cw) & 0xff);
-//                (*sendDataString).at(9) = uint8_t(uint16_t(data->cw) >> 8);
-//                // 0xBB
-//                (*sendDataString).at(10) = char(0xBB);
-                    // TODO
+                    // 0xAA,0xAdditionCmd,0xXXXX,0xYYYY,0xZZZZ,0xCWCW,0xBB
+                    sendDataString->resize(11);
+                    // make send data
+                    // 0xAA
+                    (*sendDataString).at(0) = char(0xAA);
+                    // AdditionCmd
+                    (*sendDataString).at(1) = uint8_t(data->additionCmd);
+                    // 0xXX
+                    (*sendDataString).at(2) = uint8_t(uint16_t(data->x) & 0xff);
+                    (*sendDataString).at(3) = uint8_t(uint16_t(data->x) >> 8);
+                    // 0xYY
+                    (*sendDataString).at(4) = uint8_t(uint16_t(data->y) & 0xff);
+                    (*sendDataString).at(5) = uint8_t(uint16_t(data->y) >> 8);
+                    // 0xZZ
+                    (*sendDataString).at(6) = uint8_t(uint16_t(data->z) & 0xff);
+                    (*sendDataString).at(7) = uint8_t(uint16_t(data->z) >> 8);
+                    // 0xCW
+                    (*sendDataString).at(8) = uint8_t(uint16_t(data->cw) & 0xff);
+                    (*sendDataString).at(9) = uint8_t(uint16_t(data->cw) >> 8);
+                    // 0xBB
+                    (*sendDataString).at(10) = char(0xBB);
                     // send it
                     boost::asio::async_write(
                             *(airplanePortController->sp_),
