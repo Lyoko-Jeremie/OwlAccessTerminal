7f59d49df4619941eaeed05bd52c9ed7776492d4
LyokoJeremie <lucheng989898@gmail.com>
2023-02-21 13:32:59 +0800

    ..

--- src/ConfigLoader/map_calc.js
+++ src/ConfigLoader/map_calc.js
@@ -1,4 +1,4 @@
 function calc_map_position(tagInfo) {
-    console.log("tagInfo:", JSON.stringify(tagInfo));
+    console.log("tagInfo:\n", JSON.stringify(tagInfo, undefined, 4));
     return [true, 0, 1, 2];
 }
