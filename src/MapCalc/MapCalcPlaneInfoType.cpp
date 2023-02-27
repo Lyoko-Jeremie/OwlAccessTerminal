8ecd61b22c54ed0f0291779ffce748be1bf2afa8
LyokoJeremie <lucheng989898@gmail.com>
2023-02-27 12:34:17 +0800

    MapCalcPlaneInfoType2JsonObject

---
+++ src/MapCalc/MapCalcPlaneInfoType.cpp
@@ -0,0 +1,46 @@
+// jeremie
+
+#include "MapCalcPlaneInfoType.h"
+
+namespace OwlMapCalc {
+
+    boost::json::object MapCalcPlaneInfoType2JsonObject(const std::shared_ptr<MapCalcPlaneInfoType> &info) {
+        if (!info) {
+            return {};
+        }
+        boost::json::object o{
+                {"xDirectDeg",  info->xDirectDeg},
+                {"zDirectDeg",  info->zDirectDeg},
+                {"xzDirectDeg", info->xzDirectDeg},
+                {
+                 "PlaneP",
+                                boost::json::object{
+                                        {"x", info->PlaneP.x},
+                                        {"y", info->PlaneP.y},
+                                }
+                },
+                {
+                 "ImageP",
+                                boost::json::object{
+                                        {"x", info->ImageP.x},
+                                        {"y", info->ImageP.y},
+                                }
+                },
+                {
+                 "ScaleXZ",
+                                boost::json::object{
+                                        {"x", info->ScaleXZ.x},
+                                        {"y", info->ScaleXZ.y},
+                                }
+                },
+                {
+                 "ScaleXY",
+                                boost::json::object{
+                                        {"x", info->ScaleXY.x},
+                                        {"y", info->ScaleXY.y},
+                                }
+                },
+        };
+        return 0;
+    }
+}
---
+++ src/MapCalc/MapCalcPlaneInfoType.h
@@ -0,0 +1,45 @@
+// jeremie
+
+#ifndef OWLACCESSTERMINAL_MAPCALCPLANEINFOTYPE_H
+#define OWLACCESSTERMINAL_MAPCALCPLANEINFOTYPE_H
+
+#include <memory>
+#include <boost/json.hpp>
+
+namespace OwlMapCalc {
+
+    // same as typescript `map_calc.ts` `interface PlaneInfo{}`
+    struct MapCalcPlaneInfoType : public std::enable_shared_from_this<MapCalcPlaneInfoType> {
+        struct Point2 {
+            double x;
+            double y;
+        };
+        struct Vec2 {
+            double x;
+            double y;
+        };
+        // the deg of plane X axis in image
+        double xDirectDeg;
+        // the deg of plane Z axis in image
+        double zDirectDeg;
+        // the deg of plane XZ axis (45deg) in image
+        double xzDirectDeg;
+        // Point 2 Point pair
+        // cm XZ
+        Point2 PlaneP;
+        // pixel XY
+        Point2 ImageP;
+        // Plane scale of image on Plane XZ Direct
+        // imgPixel[px]/planeDistance[1cm(SizeXY base size)]
+        Vec2 ScaleXZ;
+        // Plane scale in image XY Direct
+        // planeDistance[cm(SizeXY base size)]/imgPixel[1px]
+        Vec2 ScaleXY;
+    };
+
+    extern boost::json::object MapCalcPlaneInfoType2JsonObject(const std::shared_ptr<MapCalcPlaneInfoType> &info);
+
+}
+
+
+#endif //OWLACCESSTERMINAL_MAPCALCPLANEINFOTYPE_H
